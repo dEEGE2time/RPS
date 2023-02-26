8b23e82cc2c908f1e3fee119d51ec0eb295a338e
dEEGE2time <johannesweidensjo@hotmail.com>
2023-02-26 18:38:04 +0000

    write comments and block comments in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
+// Global variables for ease of use
 const buttons = document.getElementsByTagName('button');
 const mainMenu = document.getElementById('main-menu')
-// wait for dom to finish loading before user interaction
+const logo = document.getElementById('logo')
+
+// Wait for dom to finish loading before user interaction.
 document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute('data-type') === 'play') {
                 mainMenu.style.display = "none";
+                logo.style.display = "none"
                 startGame();
             } else if (this.getAttribute('data-type') === 'rules') {
                 mainMenu.style.display = "none";
@@ -18,8 +22,10 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 })
 
-// functions
-
+// FUNCTIONS
+/**
+ * Run the game, display game for the user.
+ */
 function startGame() {
     document.getElementById('game-window').innerHTML = `
     <div class="game-window flex-center">
@@ -38,11 +44,15 @@ function startGame() {
     </div>
     `
 }
-
+/**
+ * If user needs to read the rules, displays the rules.
+ */
 function showRules() {
 
 }
-
+/**
+ * Displays credits for content used.
+ */
 function showCredits() {
 
 }
