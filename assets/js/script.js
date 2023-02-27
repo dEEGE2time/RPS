82df16a8950feeb4385f2eadac8aa06158c58e28
dEEGE2time <johannesweidensjo@hotmail.com>
2023-02-27 23:36:28 +0000

    fix function gameOn so the game works properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const buttons = document.getElementsByTagName('button');
 const mainMenu = document.getElementById('main-menu');
 const gameWindow = document.getElementById('game-window');
 const logo = document.getElementById('logo');
-const tiles = document.getElementsByTagName('img');
+const tiles = document.getElementsByClassName('image');
 let rock = document.getElementById('rock')
 let paper = document.getElementById('paper')
 let scissor = document.getElementById('scissor')
@@ -27,8 +27,8 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-    for (let tile in tiles) {
-        tile.addEventListener("click", function() {
+    for (let i = 0; i < tiles.length; i++) {
+        tiles[i].addEventListener("click", function() {
             let playerMove = this.getAttribute('data-type');
             gameOn(playerMove);
         })
@@ -55,7 +55,7 @@ function startGame() {
  */
 function generateComputerMove() {
     let moves = ["rock", "paper", "scissor"];
-    let randomMove = Math.floor(Math.random() = moves.length);
+    let randomMove = Math.floor(Math.random() * moves.length);
     return moves[randomMove];
 }
 
@@ -64,32 +64,32 @@ function gameOn(playerMove) {
     let computerStandby = document.getElementById('computer-standby')
     let playerStandby = document.getElementById('player-standby')
     if (computerMove === "rock" && playerMove === "scissor") {
-        computerStandby = rock;
-        playerStandby = scissor;
+        computerStandby.src = "assets/images/fist.webp";
+        playerStandby.src = "assets/images/victory.webp";
         computerWin();
     } else if (computerMove === "paper" && playerMove === "rock") {
-        computerStandby = paper;
-        playerStandby = rock;
+        computerStandby.src = "assets/images/hand.webp";
+        playerStandby.src = "assets/images/fist.webp";
         computerWin();
     } else if (computerMove === "scissor" && playerMove === "paper") {
-        computerStandby = scissor;
-        playerStandby = paper;
+        computerStandby.src = "assets/images/victory.webp";
+        playerStandby.src = "assets/images/hand.webp";
         computerWin();
     } else if (computerMove === "scissor" && playerMove === "rock") {
-        computerStandby = scissor;
-        playerStandby = rock;
+        computerStandby.src = "assets/images/victory.webp";
+        playerStandby.src = "assets/images/fist.webp";
         playerWin();
     } else if (computerMove === "rock" && playerMove === "paper") {
-        computerStandby = rock;
-        playerStandby = paper;
+        computerStandby.src = "assets/images/fist.webp";
+        playerStandby.src = "assets/images/hand.webp";
         playerWin();
     } else if (computerMove === "paper" && playerMove === "scissor") {
-        computerStandby = paper;
-        playerStandby = scissor;
+        computerStandby.src = "assets/images/hand.webp";
+        playerStandby.src = "assets/images/victory.webp";
         playerWin();
     } else if (computerMove === playerMove) {
-        computerStandby = computerMove;
-        playerStandby = playerMove;
+        computerStandby.src = computerMove;
+        playerStandby.src = playerMove;
         alert('Draw!');
     }
 }
