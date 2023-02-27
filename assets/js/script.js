c81049bb97d41cd2c1ee8e956a63f697dd4f2427
dEEGE2time <johannesweidensjo@hotmail.com>
2023-02-27 22:45:56 +0000

    add functions for generating computer move and a function for game rules

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,13 @@ const buttons = document.getElementsByTagName('button');
 const mainMenu = document.getElementById('main-menu');
 const gameWindow = document.getElementById('game-window');
 const logo = document.getElementById('logo');
+const tiles = document.getElementsByTagName('img');
+let rock = document.getElementById('rock')
+let paper = document.getElementById('paper')
+let scissor = document.getElementById('scissor')
 
 // Wait for dom to finish loading before user interaction.
+// Event listeners for clicks on buttons in main menu, also in active game.
 document.addEventListener("DOMContentLoaded", function() {
     gameWindow.style.display = "none";
     for (let button of buttons) {
@@ -22,8 +27,13 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
+    for (let tile in tiles) {
+        tile.addEventListener("click", function() {
+            let playerMove = this.getAttribute('data-type');
+            gameOn(playerMove);
+        })
+    }
 })
-
 // FUNCTIONS
 /**
  * Run the game, display game for the user.
@@ -39,9 +49,51 @@ function startGame() {
     gameWindow.style.marginTop = "128px";
     gameWindow.style.width = "1024px";
     gameWindow.style.border = "5px black dashed";
-
-    
 }
+/**
+ * Generates a random computer move.
+ */
+function generateComputerMove() {
+    let moves = ["rock", "paper", "scissor"];
+    let randomMove = Math.floor(Math.random() = moves.length);
+    return moves[randomMove];
+}
+
+function gameOn(playerMove) {
+    let computerMove = generateComputerMove();
+    let computerStandby = document.getElementById('computer-standby')
+    let playerStandby = document.getElementById('player-standby')
+    if (computerMove === "rock" && playerMove === "scissor") {
+        computerStandby = rock;
+        playerStandby = scissor;
+        computerWin();
+    } else if (computerMove === "paper" && playerMove === "rock") {
+        computerStandby = paper;
+        playerStandby = rock;
+        computerWin();
+    } else if (computerMove === "scissor" && playerMove === "paper") {
+        computerStandby = scissor;
+        playerStandby = paper;
+        computerWin();
+    } else if (computerMove === "scissor" && playerMove === "rock") {
+        computerStandby = scissor;
+        playerStandby = rock;
+        playerWin();
+    } else if (computerMove === "rock" && playerMove === "paper") {
+        computerStandby = rock;
+        playerStandby = paper;
+        playerWin();
+    } else if (computerMove === "paper" && playerMove === "scissor") {
+        computerStandby = paper;
+        playerStandby = scissor;
+        playerWin();
+    } else if (computerMove === playerMove) {
+        computerStandby = computerMove;
+        playerStandby = playerMove;
+        alert('Draw!');
+    }
+}
+
 /**
  * If user needs to read the rules, displays the rules.
  */
@@ -55,11 +107,11 @@ function showCredits() {
 
 }
 
-function playerMove() {
+function playerWin() {
 
 }
 
-function computerMove() {
+function computerWin() {
 
 }
 
